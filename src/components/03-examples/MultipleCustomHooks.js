358d19ae4656cd7d1f29e6634a3c7bc905f3a099
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-20 20:24:52 -0500

    author, quotes

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -4,12 +4,33 @@ import {  Usefetch } from '../../Hooks/Usefetch';
 
 export const MultipleCustomHooks = () => {
 
-   const estado =  Usefetch( 'https://www.breakingbadapi.com/api/quotes/1' );
+    const { loading, data } =  Usefetch( 'https://www.breakingbadapi.com/api/quotes/1' );
+    const { author, quote } = !!data && data[0];
 
-    console.log( estado );
+
     return (
         <div>
-            <h1>Custom Hooks</h1>
+            <h1>BreakingBag Quotes </h1>
+
+            <hr/>
+
+            {
+                loading 
+                ?
+                (
+                    <div className="alert alert-info text-center" >
+                            <p>Loading...</p>
+                    </div>
+                )
+                :
+                (
+                    <blockquote  className="blockquote text-right">
+                        <p className="mb-0" > { quote }   </p>
+                        <footer className="blockquote-footer mt-2" >{ author }</footer>
+                    </blockquote>
+                )
+            }
         </div>
+
     )
 }
