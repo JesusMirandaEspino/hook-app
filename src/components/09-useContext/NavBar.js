25ca097327a023b362230feb7aab3a103819bb05
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-09-17 20:53:31 -0500

    navBar styles

--- src/components/09-useContext/NavBar.js
+++ src/components/09-useContext/NavBar.js
@@ -3,12 +3,20 @@ import {  Link } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
-        <nav>
-            <ul>
-                <li> <Link to="./" >Home</Link> </li>
-                <li> <Link to="./about" > About </Link> </li>
-                <li> <Link to="./login" > Login </Link> </li>
-            </ul>
+
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">Use Context</Link>
+            <div clasNames="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="./">Home</Link> </li>
+                    <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="./about">About</Link> </li>
+                    <li className="nav-item"> <Link className="nav-link" to="./login">Login</Link> </li>
+                </ul>
+            </div>
+        </div>
         </nav>
+
+
     )
 }
