8c5e74d94491378978b6857c6775feaa4099b5b5
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-09-24 21:26:52 -0500

    testing state Reducer

--- src/test/components/TodoReducer.test.js
+++ src/test/components/TodoReducer.test.js
@@ -23,7 +23,6 @@ describe('Pruebas con <TodoReducer />', () => {
 
     test('Debe de retornar el estado mas el nuevo todo', () => {
 
-
         const action = {
             type: 'add',
             payload: newTodo
@@ -34,7 +33,34 @@ describe('Pruebas con <TodoReducer />', () => {
         expect( state.length ).toBe( 3 );
         expect( state ).toEqual( [ ...demoTodos, newTodo ] );
 
-    })
+    });
 
+    test('Debe de retornar el estado menos un todo', () => {
+
+        const action = {
+            type: 'delete',
+            payload: 1
+        }
+
+        const state = TodoReducer(  demoTodos, action );
+
+        expect( state.length ).toBe( 1 );
+        expect( state ).toEqual( [demoTodos[1]] );
+
+    });
+
+
+    test('Debe de hacer TOGGLE del Todo ', () => {
+
+        const action = {
+            type: 'toggle',
+            payload: 1
+        }
+
+        const state = TodoReducer(  demoTodos, action );
+
+        expect( state[0].done).toBe( true );
+        expect( state[1] ).toEqual( demoTodos[1] );
+    });
 
 })
