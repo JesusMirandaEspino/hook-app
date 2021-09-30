14f8df21241c4fb5ea2bb742a4e47711ed0991c5
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-09-29 21:35:24 -0500

    regresa Todo mas

--- src/test/components/TodoApp.test.js
+++ src/test/components/TodoApp.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import '@testing-library/jest-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { act } from '@testing-library/react';
 
+import { demoTodos } from '../fixture/demoTodo';
 import { TodoApp } from '../../components/08-useReducer/TodoApp';
 
     const wrappers = shallow(  <TodoApp /> );
@@ -11,8 +13,21 @@ describe( 'Pruebas en  <TodoApp />', () => {
 
     test( 'Debe de mostrarse correctamente', () => {
 
-
         expect( wrappers ).toMatchSnapshot();
+
+    });
+
+
+    test( 'Debe de agregar Un TODO', () => {
+
+        const wrapper = mount( <TodoApp /> );
+
+        act( () => {
+            wrapper.find( 'TodoAdd' ).prop( 'handleAddTodo' )( demoTodos[0] );
+            wrapper.find( 'TodoAdd' ).prop( 'handleAddTodo' )( demoTodos[1] );
+        });
+
+        expect( wrapper.find( 'h1' ).text().trim() ).toBe( 'Todo App  ( 2  )' );
 
     });
 
